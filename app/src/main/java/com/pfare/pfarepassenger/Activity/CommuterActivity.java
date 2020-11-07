aa1377a20af738076c425ee042b749093dac85a2
nana yaw <nyowusukoranteng@gmail.com>
2020-11-07 14:54:48 +0000

    Added payment activity to accept payment

--- app/src/main/java/com/pfare/pfarepassenger/Activity/CommuterActivity.java
+++ app/src/main/java/com/pfare/pfarepassenger/Activity/CommuterActivity.java
@@ -1,13 +1,21 @@
 package com.pfare.pfarepassenger.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.cooltechworks.creditcarddesign.CardEditActivity;
+import com.cooltechworks.creditcarddesign.CreditCardUtils;
 import com.pfare.pfarepassenger.R;
 
 public class CommuterActivity extends AppCompatActivity {
+
+    final int GET_NEW_CARD = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +38,32 @@ public class CommuterActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Button buttonPayment = (Button)findViewById(R.id.button_pay_fare);
+        buttonPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CommuterActivity.this, CardEditActivity.class);
+                startActivityForResult(intent,GET_NEW_CARD);
+            }
+        });
     }
+
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+
+        if(resultCode == RESULT_OK) {
+
+            String cardHolderName = data.getStringExtra(CreditCardUtils.EXTRA_CARD_HOLDER_NAME);
+            String cardNumber = data.getStringExtra(CreditCardUtils.EXTRA_CARD_NUMBER);
+            String expiry = data.getStringExtra(CreditCardUtils.EXTRA_CARD_EXPIRY);
+            String cvv = data.getStringExtra(CreditCardUtils.EXTRA_CARD_CVV);
+
+            // Your processing goes here.
+
+            Toast.makeText(this, "Payment Received", Toast.LENGTH_SHORT).show();
+            finish();
+
+        }
+    }
+
 }
